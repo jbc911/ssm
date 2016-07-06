d7354604d13d3a50b77435b7ba27acff283a9343
jbc911 <jbc911@qq.com>
2016-07-06 16:53:48 +0800

    2

--- ssm/src/main/resources/b_user.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE b_user (
-	user_Id INT PRIMARY KEY auto_increment,
-	user_Name VARCHAR (40),
-	user_Password VARCHAR (40),
-	user_Level INT,
-	user_Des VARCHAR (40),
-	user_Tel VARCHAR (40),
-	user_Address VARCHAR (200)
-)
